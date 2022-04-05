ee4400a37e89ce7b0c518fd3936e2f4d1b283938
Pirahandeh <mahdipira@gmail.com>
2022-04-05 01:55:39 -0700

    ver3

--- build/project1/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ros/catkin_ws/project1_ws/src/project1
-# Build directory: /home/ros/catkin_ws/project1_ws/build/project1
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
